3c7f0047d160eb11f09388ca0d3a14abc8c4ab7b
Bruce Becker <brucellino@gmail.com>
2015-10-20 13:10:26 +0200

    style changes and added the URL for downloading. Also cleaned up some crud from the initial commit

--- build.sh
+++ build.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-SOURCE_FILE=$NAME-$VERSION.tar.gz
+SOURCE_FILE=${NAME}-${VERSION}.tar.gz
 module load ci
-echo $SOFT_DIR
-echo $WORKSPACE
-echo $SRC_DIR
+echo "SOFT_DIR is ${SOFT_DIR}
+echo "WORKSPACE is ${WORKSPACE}"
+echo "SRC_DIR is ${SRC_DIR}"
 mkdir -p $SOFT_DIR $WORKSPACE $SRC_DIR
-echo $NAME
-echo $VERSION
-module load gcc/${GCC_VERSION}
+echo "NAME is ${NAME}"
+echo "VERSION is ${VERSION}"
+module load gcc/${GCC_VERSION}"
 module load hdf5/1.8.15-gcc-${GCC_VERSION}
+module list
 
-if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
+if [[ ! -s ${SRC_DIR}/${SOURCE_FILE} ]] ; then
   echo "looks like the tarball isn't there yet"
-  ls $SRC_DIR
-  mkdir -p $SRC_DIR
-  wget  -O $SRC_DIR/$SOURCE_FILE
+  ls ${SRC_DIR}
+  mkdir -p ${SRC_DIR
+  wget  -O ${SRC_DIR}/${SOURCE_FILE} ftp://ftp.unidata.ucar.edu/pub/netcdf/old/netcdf${VERSION}
 fi
-tar -xzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+tar -xz --keep-newer-files -f ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE}
 # echo $NAME | tr '[:upper:]' '[:lower:]'
-ls $WORKSPACE
+ls ${WORKSPACE}
 # Again with the frikkin naming conventions
-cd $WORKSPACE/Python-$VERSION
-./configure --prefix=$SOFT_DIR --enable-shared
+cd $WORKSPACE/${NAME}-$VERSION
+./configure --prefix=${SOFT_DIR}-gcc-${GCC_VERSION} --enable-shared
 make -j 8
