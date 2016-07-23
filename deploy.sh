d63d62129251a5d2fd3897ff12aa4523fefac8f5
Bruce Becker <brucellino@gmail.com>
2016-07-23 10:52:28 +0200

    was missing path in modulefile

--- deploy.sh
+++ deploy.sh
@@ -69,11 +69,12 @@ proc ModulesHelp { } {
     puts stderr "       This module does nothing but alert the user"
     puts stderr "       that the [module-info name] module is not available"
 }
-prereq gmp
+module add gcc/$::env(GCC_VERSION)
 module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/netcdf-deploy"
 setenv       NETCDF_VERSION       $VERSION
 setenv       NETCDF_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION-gcc-$GCC_VERSION-mpi-$OPENMPI_VERSION
 prepend-path LD_LIBRARY_PATH   $::env(NETCDF_DIR)/lib
 prepend-path GCC_INCLUDE_DIR   $::env(NETCDF_DIR)/include
+prepend-path PATH              $::env(NETCDF_DIR)/bin
 MODULE_FILE
 ) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}-gcc-${GCC_VERSION}-mpi-${OPENMPI_VERSION}
